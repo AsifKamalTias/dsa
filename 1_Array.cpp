a238638ffbb47c673398f14589573f6cedddd888
AsifKamalTias <asifkamaltias@gmail.com>
2025-04-03 14:55:08 +0600

    pointer

--- 1_Array.cpp
+++ 1_Array.cpp
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
     // #empty array
     int a[5] = {};
-    // here the memory locations are empty out put of every index will be 0. //memory location of a index in array is Base + (i*size_of_type)
+    // here the memory locations are empty output of every index will be 0. //memory location of a index in array is Base + (i*size_of_type)
     cout << a[1] << endl; // output 0
     cout << a;            // memory location of array
 
--- 6_Pointer.cpp
+++ 6_Pointer.cpp
@@ -2,12 +2,10 @@
 using namespace std;
 int main(int argc, const char **argv)
 {
-    cout << "Hello world" << endl;
-
-    //Pointer
+    // Pointer
     int x = 10;
     cout << "Value of x: " << x << endl;
-    cout << "Address of x: " << &x << endl;
+    cout << "Address of &x: " << &x << endl;
     int *p = &x;
     cout << "Value of p: " << p << endl;
     cout << "Value of *p: " << *p << endl;
